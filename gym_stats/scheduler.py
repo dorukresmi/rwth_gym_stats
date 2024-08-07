127714c6fae72feeecae508aed9962d0bd5a452e
Doruk Resmi <dorukresmi@gmail.com>
2024-08-07 10:56:10 +0200

    added threading for properly timed checks

--- gym_stats/scheduler.py
+++ gym_stats/scheduler.py
@@ -1,6 +1,7 @@
 from gym_stats.data_store import DataStore
 import schedule
 import time
+import threading
 
 class Scheduler:
     def __init__(self, visitor_counter, db_name, table_name):
@@ -17,8 +18,21 @@ class Scheduler:
         store.add_data(image_path, number, timestamp)
         store.close()
 
+    # def start(self, interval=1):
+    #     schedule.every(interval).minutes.do(self.job)
+    #     while True:
+    #         start_time = time.time()
+    #         schedule.run_pending()
+    #         job_duration = time.time() - start_time
+    #         sleep_time = max(0, 60 - job_duration)
+    #         time.sleep(sleep_time)
+    
     def start(self, interval=1):
-        schedule.every(interval).minutes.do(self.job)
+        schedule.every(interval).minutes.do(self.job_wrapper)
         while True:
             schedule.run_pending()
             time.sleep(1)
+
+    def job_wrapper(self):
+        job_thread = threading.Thread(target=self.job)
+        job_thread.start()
